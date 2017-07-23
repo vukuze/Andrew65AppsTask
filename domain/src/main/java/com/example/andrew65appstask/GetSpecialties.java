d739b50ac808f7e0f3668fe295056b822b0b110f
andrew <kor-and@yandex.ru>
2017-07-24 01:47:21 +0400

    Промежуточный коммит для востановления случайно удаленных файлов из предыдущего коммита

---
+++ domain/src/main/java/com/example/andrew65appstask/GetSpecialties.java
@@ -0,0 +1,13 @@
+package com.example.andrew65appstask;
+
+//import com.example.andrew65appstask.db.Specialty;
+
+public class GetSpecialties {
+//    public void execute(DisposableObserver<List<Specialty>> observer) {
+//        Preconditions.checkNotNull(observer);
+//        final Observable<T> observable = this.buildUseCaseObservable(params)
+//                .subscribeOn(Schedulers.from(threadExecutor))
+//                .observeOn(postExecutizonThread.getScheduler());
+//        addDisposable(observable.subscribeWith(observer));
+//    }
+}
